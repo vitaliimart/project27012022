be2baf2b1b079cc8d1add19da067a22c136d36d6
Vitalii <vitalii.martych@gmail.com>
2022-01-30 13:10:03 +0200

    add info about methods for numbers

--- script.js
+++ script.js
@@ -264,29 +264,36 @@
 // console.log(calc(5,3));
 // console.log(calc(6,3));
 
-const str = 'test';
-const arr = [1, 2, 3];
+// const str = 'test';
+// const arr = [1, 2, 3];
 
-console.log(arr.length);
-console.log(str.length);
-console.log(str[2]);
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
+// console.log(arr.length);
+// console.log(str.length);
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
 
-const fruit = 'Some fruit';
-console.log(fruit.indexOf('fruit'));
-console.log(fruit.indexOf('q'));
+// const fruit = 'Some fruit';
+// console.log(fruit.indexOf('fruit'));
+// console.log(fruit.indexOf('q'));
 
-const logg = 'Hello world';
-console.log(logg.slice(6, 11));
-console.log(logg.length);
+// const logg = 'Hello world';
+// console.log(logg.slice(6, 11));
+// console.log(logg.length);
 
-console.log(logg.slice(6, logg.length));
-console.log(logg.slice(6));
-console.log(logg.slice(-5));
+// console.log(logg.slice(6, logg.length));
+// console.log(logg.slice(6));
+// console.log(logg.slice(-5));
 
-console.log(logg.slice(-1));
-console.log(logg.slice(0, 5));
+// console.log(logg.slice(-1));
+// console.log(logg.slice(0, 5));
 
-console.log(logg.substring(6, 11));
-console.log(logg.substr(6, 5));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));

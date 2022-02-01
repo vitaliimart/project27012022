ce1986dafb2dbbbcaef7f64fa00cbca8cfdfa3d1
Vitalii <vitalii.martych@gmail.com>
2022-02-01 22:23:07 +0200

    додав сучасні та застарілі методи отримання елементів зі сторінки веб-сайту

--- script.js
+++ script.js
@@ -519,3 +519,29 @@
 // console.log(0 || "" || 2 || undefined || true || false);
 
 // Отримання елементів зі сторінки
+// Додав верстку та стилі до верстки
+
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+
+console.log(btns);
+
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+console.log(hearts);
+
+hearts.forEach(item => {
+	console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+

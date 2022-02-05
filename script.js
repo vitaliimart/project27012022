d9332ed42e992cef82bb1f63fc0de085bc48c8b8
Vitalii <vitalii.martych@gmail.com>
2022-02-05 23:29:34 +0200

    події на мобільних додатках

--- script.js
+++ script.js
@@ -666,3 +666,43 @@ for (let node of document.body.childNodes) {
 }
 
 // ----------------------------------
+// події на мобільних додатках
+// touchstart
+// touchmove
+// touchend
+//// touchenter
+//// touchleave
+//// touchcansel
+
+window.addEventListener('DOMContentLoaded', () => {
+	const box = document.querySelector('.box');
+
+	box.addEventListener('touchstart', (e) => {
+		e.preventDefault();
+
+		console.log('Start');
+		console.log(e.touches);
+		console.log(e.targetTouches);
+		console.log(e.changedTouches);
+	});
+
+	box.addEventListener('touchmove', (e) => {
+		e.preventDefault();
+
+		console.log('Move');
+	});
+
+	box.addEventListener('touchend', (e) => {
+		e.preventDefault();
+
+		console.log('End');
+	});
+
+	box.addEventListener('touchleave', (e) => {
+		e.preventDefault();
+
+		console.log('Leave');
+	});
+});
+
+//--------------------------------------------------------------------------------------

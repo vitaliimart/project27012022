816bef7284665e6d3ce20ae6b9d0cb964bc396df
Vitalii <vitalii.martych@gmail.com>
2022-02-08 21:56:22 +0200

    add info about script execution tome management

--- js/script.js
+++ js/script.js
@@ -1,45 +1,65 @@
-const btns = document.querySelectorAll('button'),
-			wrapper = document.querySelector('.btn-block');
-// console.log(btns[0].classList.item(0));
-// console.log(btns[0].classList.length);
-// console.log(btns[1].classList.add('red', 'tttttttt'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
+// const timerId = setTimeout(function(text) {
+// 	console.log('text');
+// }, 2000, 'Hello');
 
-// if(btns[1].classList.contains('red')) {
-// 	console.log('red');
+// const timerId = setTimeout(logger, 2000);
+
+// function logger() {
+// 	console.log('text');
 // }
-btns[0].addEventListener('click', () => {
-	// if(!btns[1].classList.contains('red')) {
-	// 	btns[1].classList.add('red');
-	// } else {
-	// 	btns[1].classList.remove('red');
-	// }
-	btns[1].classList.toggle('red');
-});
-
-wrapper.addEventListener('click', (event) => {
-	// if (event.target && event.target.tagName == 'BUTTON') {
-	// 	console.log('Hello');
-	// }
-
-	if (event.target && event.target.matches('button.green')) {
-		console.log('Hello');
-	}
-
-		// if (event.target && event.target.classList.contains('blue')) {
-		// console.log('Hello');
-	// }
-});
 
 
-// btns.forEach(btn => {
-// 	btn.addEventListener('click', () => {
-// 		console.log('Hello');
-// 	});
+
+// setTimeout(logger, 2000);
+
+// function logger() {
+// 	console.log('text');
+// }
+
+
+// const timerId = setTimeout(logger, 2000);
+// clearInterval(timerId);
+// function logger() {
+// 	console.log('text');
+// }
+
+const btn = document.querySelector('.btn');
+let	timerId,
+		i = 0;
+
+// btn.addEventListener('click', () => {
+// 	// const timerId = setTimeout(logger, 2000);
+// 	timerId = setInterval(logger, 500);
 // });
 
-const btn = document.createElement('button');
-btn.classList.add('green');
-wrapper.append(btn);
-// wrapper.prepend(btn);
+// function logger() {
+// 	if (i === 3) {
+// 		clearInterval(timerId);
+// 	}
+// 		console.log('text');
+// 	i++;
+// }
+
+// let id = setTimeout(function log() {
+// 	console.log('Hello');
+// 	id = setTimeout(log, 500);
+// }, 500);
+
+
+function myAnimation() {
+	const elem = document.querySelector('.box');
+	let pos = 0;
+
+	const id = setInterval(frame, 10);
+	function frame() {
+		if (pos == 300) {
+			clearInterval(id);
+		} else {
+			pos++;
+			elem.style.top = pos + 'px';
+			elem.style.left = pos + 'px';
+		}
+	}
+}
+
+btn.addEventListener('click', myAnimation);
